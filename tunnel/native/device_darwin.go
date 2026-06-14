b99816f6ca712a68c21dd0eef6f1f6833e7469f9
oukishu <89119588+oukishu@users.noreply.github.com>
2026-06-14 12:24:50 +0800

    Update device_darwin.go

--- tunnel/native/device_darwin.go
+++ tunnel/native/device_darwin.go
@@ -6,66 +6,80 @@ import (
 	"context"
 	"encoding/binary"
 	"fmt"
+	"net"
 	"net/netip"
 	"os"
-	"os/exec"
-	"strconv"
-	"strings"
 	"syscall"
 	"unsafe"
 
-	"github.com/oukishu/onet/tun"
+	"github.com/oukishu/onet/tunnel"
+	"golang.org/x/sys/unix"
 )
 
 const (
-	darwinAFSystem        = 32
-	darwinAFSysControl    = 2
-	darwinSysProtoControl = 2
-	darwinCtlIOCGInfo     = 0xc0644e03
+	SIOCAIFADDR_IN6       = 2155899162 // netinet6/in6_var.h
+	IN6_IFF_NODAD         = 0x0020     // netinet6/in6_var.h
+	IN6_IFF_SECURED       = 0x0400     // netinet6/in6_var.h
+	ND6_INFINITE_LIFETIME = 0xFFFFFFFF // netinet6/nd6.h
 )
+
+const utunControlName = "com.apple.net.utun_control"
+
+type ifAliasReq struct {
+	Name    [unix.IFNAMSIZ]byte
+	Addr    unix.RawSockaddrInet4
+	Dstaddr unix.RawSockaddrInet4
+	Mask    unix.RawSockaddrInet4
+}
+
+type ifAliasReq6 struct {
+	Name     [16]byte
+	Addr     unix.RawSockaddrInet6
+	Dstaddr  unix.RawSockaddrInet6
+	Mask     unix.RawSockaddrInet6
+	Flags    uint32
+	Lifetime addrLifetime6
+}
+
+type addrLifetime6 struct {
+	Expire    float64
+	Preferred float64
+	Vltime    uint32
+	Pltime    uint32
+}
 
 type darwinDriver struct {
 	name string
 	file *os.File
 }
 
-type ctlInfo struct {
-	id   uint32
-	name [96]byte
-}
+func openPlatform(_ context.Context, config tunnel.Config) (platformDriver, error) {
+	ifIndex := -1
+	if _, err := fmt.Sscanf(config.Name, "utun%d", &ifIndex); err != nil {
+		return nil, fmt.Errorf("bad tun name: %s", config.Name)
+	}
 
-type sockaddrCtl struct {
-	len      uint8
-	family   uint8
-	sysaddr  uint16
-	id       uint32
-	unit     uint32
-	reserved [5]uint32
-}
-
-func openPlatform(_ context.Context, config tun.Config) (platformDriver, error) {
-	fd, err := syscall.Socket(darwinAFSystem, syscall.SOCK_DGRAM, darwinSysProtoControl)
+	fd, err := unix.Socket(unix.AF_SYSTEM, unix.SOCK_DGRAM, 2 /* SYSPROTO_CONTROL */)
 	if err != nil {
 		return nil, err
 	}
-	info := ctlInfo{}
-	copy(info.name[:], "com.apple.net.utun_control")
-	if _, _, errno := syscall.Syscall(syscall.SYS_IOCTL, uintptr(fd), uintptr(darwinCtlIOCGInfo), uintptr(unsafe.Pointer(&info))); errno != 0 {
-		_ = syscall.Close(fd)
-		return nil, errno
+
+	ctlInfo := &unix.CtlInfo{}
+	copy(ctlInfo.Name[:], utunControlName)
+	if err = unix.IoctlCtlInfo(fd, ctlInfo); err != nil {
+		unix.Close(fd)
+		return nil, os.NewSyscallError("IoctlCtlInfo", err)
 	}
-	unit, name := darwinUnit(config.Name)
-	addr := sockaddrCtl{
-		len:     uint8(unsafe.Sizeof(sockaddrCtl{})),
-		family:  darwinAFSystem,
-		sysaddr: darwinAFSysControl,
-		id:      info.id,
-		unit:    unit,
+
+	if err = unix.Connect(fd, &unix.SockaddrCtl{
+		ID:   ctlInfo.Id,
+		Unit: uint32(ifIndex) + 1,
+	}); err != nil {
+		unix.Close(fd)
+		return nil, os.NewSyscallError("Connect", err)
 	}
-	if _, _, errno := syscall.Syscall(syscall.SYS_CONNECT, uintptr(fd), uintptr(unsafe.Pointer(&addr)), uintptr(addr.len)); errno != 0 {
-		_ = syscall.Close(fd)
-		return nil, errno
-	}
+
+	name := fmt.Sprintf("utun%d", ifIndex)
 	file := os.NewFile(uintptr(fd), name)
 	return &darwinDriver{name: name, file: file}, nil
 }
@@ -74,28 +88,109 @@ func (d *darwinDriver) Start() error   { return nil }
 func (d *darwinDriver) Close() error   { return d.file.Close() }
 func (d *darwinDriver) Name() string   { return d.name }
 func (d *darwinDriver) File() *os.File { return d.file }
-func (d *darwinDriver) Configure(config tun.Config) error {
-	if err := exec.Command("ifconfig", d.name, "mtu", strconv.Itoa(config.MTU), "up").Run(); err != nil {
-		return err
+
+func (d *darwinDriver) Configure(config tunnel.Config) error {
+	fd := int(d.file.Fd())
+
+	// Set MTU
+	err := useSocket(unix.AF_INET, unix.SOCK_DGRAM, 0, func(socketFd int) error {
+		var ifr unix.IfreqMTU
+		copy(ifr.Name[:], d.name)
+		ifr.MTU = int32(config.MTU)
+		return unix.IoctlSetIfreqMTU(socketFd, &ifr)
+	})
+	if err != nil {
+		return os.NewSyscallError("IoctlSetIfreqMTU", err)
 	}
+
+	_ = fd // fd used implicitly via d.file for name resolution above
+
+	// Configure IPv4 addresses
 	for _, address := range config.Inet4Address {
-		addr, mask := ipv4Ifconfig(address)
-		if err := exec.Command("ifconfig", d.name, "inet", addr, addr, "netmask", mask).Run(); err != nil {
+		ifReq := ifAliasReq{
+			Addr: unix.RawSockaddrInet4{
+				Len:    unix.SizeofSockaddrInet4,
+				Family: unix.AF_INET,
+				Addr:   address.Addr().As4(),
+			},
+			Dstaddr: unix.RawSockaddrInet4{
+				Len:    unix.SizeofSockaddrInet4,
+				Family: unix.AF_INET,
+				Addr:   address.Addr().As4(),
+			},
+			Mask: unix.RawSockaddrInet4{
+				Len:    unix.SizeofSockaddrInet4,
+				Family: unix.AF_INET,
+				Addr:   netip.MustParseAddr(net.IP(net.CIDRMask(address.Bits(), 32)).String()).As4(),
+			},
+		}
+		copy(ifReq.Name[:], d.name)
+		err = useSocket(unix.AF_INET, unix.SOCK_DGRAM, 0, func(socketFd int) error {
+			if _, _, errno := unix.Syscall(
+				syscall.SYS_IOCTL,
+				uintptr(socketFd),
+				uintptr(unix.SIOCAIFADDR),
+				uintptr(unsafe.Pointer(&ifReq)),
+			); errno != 0 {
+				return os.NewSyscallError("SIOCAIFADDR", errno)
+			}
+			return nil
+		})
+		if err != nil {
 			return err
 		}
 	}
+
+	// Configure IPv6 addresses
 	for _, address := range config.Inet6Address {
-		if err := exec.Command("ifconfig", d.name, "inet6", address.String(), "prefixlen", strconv.Itoa(address.Bits())).Run(); err != nil {
+		ifReq6 := ifAliasReq6{
+			Addr: unix.RawSockaddrInet6{
+				Len:    unix.SizeofSockaddrInet6,
+				Family: unix.AF_INET6,
+				Addr:   address.Addr().As16(),
+			},
+			Mask: unix.RawSockaddrInet6{
+				Len:    unix.SizeofSockaddrInet6,
+				Family: unix.AF_INET6,
+				Addr:   netip.MustParseAddr(net.IP(net.CIDRMask(address.Bits(), 128)).String()).As16(),
+			},
+			Flags: IN6_IFF_NODAD | IN6_IFF_SECURED,
+			Lifetime: addrLifetime6{
+				Vltime: ND6_INFINITE_LIFETIME,
+				Pltime: ND6_INFINITE_LIFETIME,
+			},
+		}
+		if address.Bits() == 128 {
+			ifReq6.Dstaddr = unix.RawSockaddrInet6{
+				Len:    unix.SizeofSockaddrInet6,
+				Family: unix.AF_INET6,
+				Addr:   address.Addr().Next().As16(),
+			}
+		}
+		copy(ifReq6.Name[:], d.name)
+		err = useSocket(unix.AF_INET6, unix.SOCK_DGRAM, 0, func(socketFd int) error {
+			if _, _, errno := unix.Syscall(
+				syscall.SYS_IOCTL,
+				uintptr(socketFd),
+				uintptr(SIOCAIFADDR_IN6),
+				uintptr(unsafe.Pointer(&ifReq6)),
+			); errno != 0 {
+				return os.NewSyscallError("SIOCAIFADDR_IN6", errno)
+			}
+			return nil
+		})
+		if err != nil {
 			return err
 		}
 	}
+
 	return nil
 }
 
-func (d *darwinDriver) Read(ctx context.Context) (tun.Packet, error) {
+func (d *darwinDriver) Read(ctx context.Context) (tunnel.Packet, error) {
 	packet, err := readRaw(ctx, d.file)
 	if err != nil {
-		return tun.Packet{}, err
+		return tunnel.Packet{}, err
 	}
 	if len(packet.Payload) >= 4 {
 		packet.Payload = packet.Payload[4:]
@@ -103,7 +198,7 @@ func (d *darwinDriver) Read(ctx context.Context) (tun.Packet, error) {
 	return packet, nil
 }
 
-func (d *darwinDriver) Write(ctx context.Context, packet tun.Packet) error {
+func (d *darwinDriver) Write(ctx context.Context, packet tunnel.Packet) error {
 	family := make([]byte, 4)
 	if len(packet.Payload) > 0 && packet.Payload[0]>>4 == 6 {
 		binary.BigEndian.PutUint32(family, syscall.AF_INET6)
@@ -114,19 +209,11 @@ func (d *darwinDriver) Write(ctx context.Context, packet tun.Packet) error {
 	return writeRaw(ctx, d.file, packet)
 }
 
-func darwinUnit(name string) (uint32, string) {
-	if !strings.HasPrefix(name, "utun") {
-		return 0, "utun"
+func useSocket(domain, typ, proto int, block func(socketFd int) error) error {
+	socketFd, err := unix.Socket(domain, typ, proto)
+	if err != nil {
+		return err
 	}
-	n, err := strconv.Atoi(strings.TrimPrefix(name, "utun"))
-	if err != nil || n < 0 {
-		return 0, "utun"
-	}
-	return uint32(n + 1), fmt.Sprintf("utun%d", n)
-}
-
-func ipv4Ifconfig(prefix netip.Prefix) (string, string) {
-	addr := prefix.Addr().String()
-	mask := uint32(0xffffffff) << uint(32-prefix.Bits())
-	return addr, fmt.Sprintf("0x%08x", mask)
+	defer unix.Close(socketFd)
+	return block(socketFd)
 }
